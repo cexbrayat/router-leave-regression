dc53610e60155e8817fa788ebb1e7dba332fe06b
cexbrayat <cedric@ninja-squad.com>
2020-07-07 08:38:51 +0200

    feat: add simple leave guard

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,25 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <h1>Hello</h1>
+  <RouterLink to="/other">Trigger leave guard</RouterLink>
 </template>
 
 <script>
+import { defineComponent, ref } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
+  setup() {
+    const saved = ref(false);
+    onBeforeRouteLeave((to, from, next) => {
+      if (!saved.value) {
+        const confirmation = confirm('Are you sure you want to leave the current page?');
+        if (!confirmation) {
+          next(false);
+        } else {
+          next();
+        }
+      }
+    });
   }
 }
 </script>
