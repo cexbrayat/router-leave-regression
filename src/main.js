a1c037059b176e3112af2da7c255876c8710ece3
cexbrayat <cedric@ninja-squad.com>
2020-07-07 08:34:38 +0200

    feat: setup router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'hello',
+      component: HelloWorld
+    }
+  ]
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
